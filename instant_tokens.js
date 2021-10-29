a5cbd012e06975683a22928ba243bcdd23829dcc
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2021-10-28 20:50:17 -0400

    Update instant_tokens.js

--- instant_tokens.js
+++ instant_tokens.js
@@ -1,7 +1,9 @@
 async function getName(authToken) {
     const response = await fetch('https://api.blooket.com/api/users/verify-token?token=JWT+' + authToken);
     const data = await response.json();
-    alert(data)
+    if(data!=null){
+        alert(data.name)
+    }
     
     return data.name
 };
