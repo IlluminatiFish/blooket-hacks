044c7d0d71b3b8b4e80048dfd55ebb0d98a76d7b
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2021-10-29 20:51:30 -0400

    Update auto_answer.js

--- auto_answer.js
+++ auto_answer.js
@@ -1,3 +1,8 @@
+async function getName(authToken) {
+    const response = await fetch('https://api.blooket.com/api/users/verify-token?token=JWT+' + authToken);
+    const data = await response.json();
+    return data.name
+};
 function fetchApi(url, json, method){
 	fetch(url, {
   	method: method,
